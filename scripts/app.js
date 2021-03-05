cb8af594172d625acac52afcf616c1efded1d4ad
Ryan Etten <retten21127@columbushs.com>
2021-03-05 13:29:37 -0500

    weather app done

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,43 @@
 const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
+
+const updateUI = (data) => {
+    // destructure properties
+    const { cityDets, weather } = data;
+
+    // update details template
+    details.innerHTML = `
+    <h5 class="my-3">${cityDets.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+        <span>${weather.Temperature.Metric.Value}</span>
+        <span>&deg;C</span>
+    </div>
+    `;
+
+    // update night/day & icon images
+    const iconSrc = `icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute('src', iconSrc);
+
+    let timeSrc = weather.IsDayTime ? 'icons/daytime.jpg' : 'icons/nighttime.jpg';
+    time.setAttribute('src', timeSrc);
+
+    // remove the d-none class if present
+    if(card.classList.contains('d-none')){
+        card.classList.remove('d-none');
+    }
+
+};
 
 const updateCity = async (city) => {
 
     const cityDets = await getCity(city);
     const weather = await getWeather(cityDets.Key);
 
-    return {
-        cityDets: cityDets,
-        weather: weather
-    };
+    return { cityDets, weather };
 
 };
 
@@ -21,5 +50,10 @@ cityForm.addEventListener('submit', e => {
     cityForm.reset();
 
     // update the UI with new city
-    updateCity(city).then(data => console.log(data)).catch(err => console.log(err));
+    updateCity(city)
+        .then(data => updateUI(data))
+        .catch(err => console.log(err));
 });
+
+const result = true ? 'value 1' : 'value 2';
+console.log(result);
